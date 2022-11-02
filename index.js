927b174bfafe0c7c5a502c2ee84dc341f51d51e6
rismacantikkkk10 <rismarismaaa975@gmail.com>
2022-11-02 12:32:06 +0800

    Risma Tugas percabangan

---
+++ index.js
@@ -0,0 +1,24 @@
+var pendapatan = 300000;
+var komisi, jasa, total;
+
+if (pendapatan >= 0 && pendapatan <= 200000){
+    jasa = 10000;
+    komisi = 0.1*pendapatan;
+}
+else if (pendapatan <= 500000)
+{
+    jasa = 200000;
+    komisi = 0.15*pendapatan;
+}
+else
+{
+    jasa = 300000;
+    komisi = 0.2;
+}
+{
+    total = komisi + jasa;
+    Console.log(jasa);
+    Console.log(komisi)
+    Console.log(total)
+
+}
